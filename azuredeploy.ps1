9f3a5d36c21843a4133c15b3cd2436f4d166f285
Mark Marquez <marquezemark@ucsolutions.tech>
2019-02-17 09:02:15 +0200

    Replaced ALL URL

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -35,7 +35,7 @@ $parameters = @{ `
     adminPassword= "@Passw0rd123"
     domainName="mydomain.local"
     dnsPrefix = "sfblab"+$random
-    _artifactsLocation = "https://raw.githubusercontent.com/ibenbouzid/SkypeforBusiness_lab_V2/master"
+    _artifactsLocation = "https://raw.githubusercontent.com/MarquezEMark/SkypeforBusinessLab/Branch1"
     _SfBshareLocation= "your storage account name (not the path) that contains skype executable files"
     _SfBshareSasToken = "the token to access that share"
     }
@@ -54,8 +54,8 @@ Write-Host "Elapsed Deployment Time for '$ResourceGroupName' :'$Duration' "
 # Deploy the lab using the parameterfile##############################################################################
 $random=Get-Random -minimum 1000 -maximum 9999
 $ResourceGroupName = "sfblab2"
-$TemplateUri = "https://raw.githubusercontent.com/ibenbouzid/SkypeforBusiness_lab_V2/master/azuredeploy.json"
-$TemplateParametersUri = "https://raw.githubusercontent.com/ibenbouzid/SkypeforBusiness_lab_V2/master/azuredeploy.parameters.json"
+$TemplateUri = "https://raw.githubusercontent.com/MarquezEMark/SkypeforBusinessLab/Branch1/azuredeploy.json"
+$TemplateParametersUri = "https://raw.githubusercontent.com/MarquezEMark/SkypeforBusinessLab/Branch1/azuredeploy.parameters.json"
 
 $StartTime = Get-Date
 Write-Host "Starting deployment of resource group : '$ResourceGroupName' on '$StartTime'"  
